f2c4e6c43332e375ab3e697837c72ab4e79b268c
achierichie <achie.richie78@gmail.com>
2021-01-15 00:43:52 +0800

    dataForm

---
+++ app/src/main/java/com/koa/dataform/MainActivity.java
@@ -0,0 +1,40 @@
+package com.koa.dataform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+//    TextInputLayout dropdown;
+//    AutoCompleteTextView act_season;
+//    ArrayList<String> arrayList_dropdown;
+//    ArrayAdapter<String> arrayAdapter_dropdown;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        dropdown=(TextInputLayout)findViewById(R.id.dropdown);
+//        act_season=(AutoCompleteTextView)findViewById(R.id.act_season);
+//
+//        arrayList_dropdown=new ArrayList<>();
+//        arrayList_dropdown.add("Good");
+//        arrayList_dropdown.add("Moderate");
+//        arrayList_dropdown.add("Bad");
+//
+//
+//        arrayAdapter_dropdown=new ArrayAdapter<>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,arrayList_dropdown);
+//        act_season.setAdapter(arrayAdapter_dropdown);
+//
+//        act_season.setThreshold(0);
+
+    }
+}
