7df35547fbcfa242a90528a09b94a4013bd526b1
Tony Marfo O <marfotony@gmail.com>
2021-01-21 09:18:33 +0000

    Date picker update

--- app/src/main/java/com/koa/dataform/MainActivity.java
+++ app/src/main/java/com/koa/dataform/MainActivity.java
@@ -3,7 +3,10 @@ package com.koa.dataform;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatCheckBox;
 import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -11,9 +14,12 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -21,11 +27,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Date date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
         //    ArrayAdapter<String> arrayAdapter_dropdown;
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_hhmm", Locale.ENGLISH);
         Button saveButton = findViewById(R.id.saveBtn);
+        TextInputEditText dateInput = findViewById(R.id.dateField);
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        dateInput.setText(sdf2.format(new Date()));
+        dateInput.setOnFocusChangeListener((v,a) -> {
+            if(a) {
+                showDatePickerDialog(dateInput);
+            }
+        });
 
         if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
             saveButton.setEnabled(false);
@@ -44,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
         saveButton.setOnClickListener(v -> {
             //get the values from the form
-            String name = ((TextInputLayout)findViewById(R.id.textInputLayout4)).getEditText().getText().toString().trim();
+//            String name = ((TextInputLayout)findViewById(R.id.textInputLayout4)).getEditText().getText().toString().trim();
             String enumerator = ((TextInputLayout)findViewById(R.id.textInputLayout6)).getEditText().getText().toString().trim();
             String farmerName = ((TextInputLayout)findViewById(R.id.textInputLayout7)).getEditText().getText().toString().trim();
             String farmLocation = ((TextInputLayout)findViewById(R.id.textInputLayout8)).getEditText().getText().toString().trim();
@@ -71,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
             String fileName = "Farm_Assessment_Form_" + sdf.format(new Date())+ ".csv";
             try {
                 ArrayList<String> values = new ArrayList<>();
-                values.add(name);
+//                values.add(name);
                 values.add(enumerator);
                 values.add(farmerName);
                 values.add(farmLocation);
@@ -113,6 +129,34 @@ public class MainActivity extends AppCompatActivity {
     private static boolean isExternalStorageAvailable() {
         String extStorageState = Environment.getExternalStorageState();
         return Environment.MEDIA_MOUNTED.equals(extStorageState);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+            Calendar c = Calendar.getInstance();
+            c.set(year, month, day);
+            Date date = c.getTime();
+            ((TextInputEditText)getActivity().findViewById(R.id.dateField)).setText(sdf.format(date));
+        }
     }
 
 }
